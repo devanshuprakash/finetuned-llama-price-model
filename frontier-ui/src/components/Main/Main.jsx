f626880a71754a41d4cc2ce787e11b0a5f2f732a
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-02-18 16:36:44 +0530

    fine-tuning openai

--- frontier-ui/src/components/Main/Main.jsx
+++ frontier-ui/src/components/Main/Main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="main">
+      <div className="nav">
+        <p>Forntier-ui </p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <p><span>Hello,user</span></p>
+      </div>
+    </div>
   )
 }
 
